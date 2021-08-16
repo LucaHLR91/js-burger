feb8976f6e9f62b61212a32ea93b715b8c572633
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-08-16 12:23:19 +0200

    Customer Name

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 // Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
+
+//RICHIEDO IL NOME DEL CLIENTE
+var customerName = prompt('Inserisci il tuo nome');
+document.getElementById('customer-name').innerHTML = customerName;
