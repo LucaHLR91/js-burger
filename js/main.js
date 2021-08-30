519e158187a83e37c8a88ef6426a9d0a26cda977
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-08-30 14:38:42 +0200

    Indentazione

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 // Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
 
-//RICHIEDO IL NOME DEL CLIENTE
+//RICHIEDO IL NOME DEL PANINO
 var customerBurgerName = prompt('Dai un nome al tuo panino');
 document.getElementById('customer-name').innerHTML = customerBurgerName;
 
 //PREZZO BASE HAMBURGER
 let hamburgerPrice = 5;
 document.getElementById('final-price').innerHTML = "€" + " " + hamburgerPrice;
+
+//ARRAY CODICI SCONTO
+let discountCode = ['12345ABCDE', 'ABCDE12345', '1A2B3C4D5E'];
+
 //FUNZIONI
