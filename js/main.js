24c9589bf3c0d791985cee3a3b02c7db44e1efaf
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-07 16:56:34 +0200

    Burger new

--- js/main.js
+++ js/main.js
@@ -1,14 +1,70 @@
-// Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
+// SALVIAMO IN UNA VARIABILE IL NOSTRO BOTTONE
+var btn = document.getElementById('button');
+console.log(btn);
 
-//RICHIEDO IL NOME DEL PANINO
-var customerBurgerName = prompt('Dai un nome al tuo panino');
-document.getElementById('customer-name').innerHTML = customerBurgerName;
+// SALVIAMO IN UNA VARIABILE I NOSTRI ADD
+var add = document.getElementsByClassName('ingredient-add');
+console.log(add);
 
-//PREZZO BASE HAMBURGER
-let hamburgerPrice = 5;
-document.getElementById('final-price').innerHTML = "€" + " " + hamburgerPrice;
+for (var i = 0; i < add.length; i++) {
+    const thisAdd = add[i];
+    thisAdd.addEventListener('click',
+        function () {
+            console.log(thisAdd.previousElementSibling);
 
-//ARRAY CODICI SCONTO
-let discountCode = ['12345ABCDE', 'ABCDE12345', '1A2B3C4D5E'];
+            thisAdd.previousElementSibling.checked = !thisAdd.previousElementSibling.checked
+        }
+    );
+}
 
-//FUNZIONI
+// RIMANIAMO IN ASCOLTO DI UN EVENTO CLICK SU QUEL BOTTONE
+btn.addEventListener('click',
+    function () {
+        //quando il bottone sarà cliccato sarà avviato questo codice
+
+        // prendiamo il valore dell'input nome
+        var name = document.getElementById('name').value;
+        console.log(name);
+
+        //se non abbiamo un nome non possiamo procedere
+        if (name.length === 0) {
+            alert('non hai inserito un nome');
+        } else {
+            // prendiamo il valore dai checkbox
+            var checkboxes = document.getElementsByClassName('ingredient-checkbox');
+            console.log(checkboxes);
+
+            // prendiamo i singoli valori e sommiamoli al prezzo base
+            var sum = 50;
+            for (var i = 0; i < checkboxes.length; i++) {
+                var thisCheckbox = checkboxes[i];
+                console.log(thisCheckbox.checked);
+
+                if (thisCheckbox.checked) {
+                    sum += parseInt(thisCheckbox.value);
+                }
+            }
+
+            console.log(sum);
+
+            // prendiamo il valore del coupon
+            var coupon = document.getElementById('coupon').value;
+            var coupons = [
+                '123456ABCDEF',
+                '59478HUIDDIK',
+                '86921UTVDNIO'
+            ];
+
+            // controlliamo che sia un codice valido
+            if (coupons.includes(coupon)) {
+                console.log('coupon valido');
+                //applichiamo 20% di sconto
+                sum = sum - (sum / 100 * 20);
+            }
+
+            // stampiamo il prezzo
+            var elPrice = document.getElementById('price');
+            elPrice.innerHTML = sum;
+        }
+    }
+);
